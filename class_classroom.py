7c7bd139eed2c804d33a46991ac69bd3f720f795
Colfu <colfu@Colfus-MacBook-Air.local>
2021-09-01 12:15:02 +0100

    Testing and updating class_classroom functions.

--- class_classroom.py
+++ class_classroom.py
@@ -1,3 +1,5 @@
+# Q: How to connect this to the Students made using Student Class
+#  need to change add_stdent() to use Student Class objects?
 class Classroom:
     """
     Create a class Classroom that keeps records of students (as objects of the
@@ -10,8 +12,8 @@ class Classroom:
     in that classroom
 
     function: add_current_classroom_size() - replace with get_current_room_size()
-    function: print_students_average_grades(): print the average grade of each student that belongs to the classroom
-    function: print_classroom_average_grade(): print the total average grade of the class
+    function: get_students_average_grades(): the average grade of each student that belongs to the classroom
+    function: get_classroom_average_grade(): the total average grade of the class
     function: add_student_to_classroom(): add student entry
     function: remove_student_from_classroom(): remove student by providing the first name and the last name
     function: get_classroom_id(): get the classroom id
@@ -22,30 +24,44 @@ class Classroom:
         self.classroom_id = classroom_id
         self.max_classroom_size = max_classroom_size
         self.current_classroom_size = 0
-        self.register_list = [{'Firstname': None, 'Lastname': None, 'Average Grade': None}]
+        self.register_list = []
 
     def add_current_room_size(self, current_size):
         # Not needed - see get_current_room_size function which is a better way of getting the information
         self.current_classroom_size = current_size
 
     def get_current_classroom_size(self):
-        return len(self.register_list)
+        if len(self.register_list) == 0:
+            return 'Class Empty'
+        else:
+            return len(self.register_list)
 
-    def print_students_average_grades(self):
-        return self.register_list
+    def get_students_average_grades(self):
+        for student_dict in self.register_list:
+            fn = student_dict.get('Firstname')
+            ln = student_dict.get('Lastname')
+            avg = student_dict.get('Average Grade')
+            print(f'{fn} {ln}, Av.Grade: {avg}')
+        return ' '
 
-    def print_classroom_average_grade(self):
+    def get_classroom_average_grade(self):
         total = 0
         for student_dict in self.register_list:
-            total += student_dict['Average Grade']
+            if student_dict.get('Average Grade') is None:
+                continue
+            else:
+                total += student_dict.get('Average Grade')
         return total/len(self.register_list)
 
     def add_student(self, firstname, lastname):
-        student_dict = {'Firstname': firstname, 'Lastname': lastname, 'Average Grade': None}
-        self.register_list.append(student_dict)
+        # Adding a student to the register, unless class is already full
+        if self.current_classroom_size == self.max_classroom_size:
+            print('Sorry, this class is already full.')
+        else:
+            student_dict = {'Firstname': firstname, 'Lastname': lastname, 'Average Grade': None}
+            self.register_list.append(student_dict)
 
     def remove_student_from_classroom(self, firstname, lastname):
-        #
         for student_dict in self.register_list:
             if student_dict.get('Firstname') == firstname and student_dict.get('Lastname') == lastname:
                 self.register_list.remove(student_dict)
@@ -57,4 +73,10 @@ class Classroom:
 
 # Test Data:
 # room_01 = Classroom("room_01", 25)
-# room_02 = Classroom("room_02", 40)
+# # room_02 = Classroom("room_02", 40)
+#
+# room_01.add_student('Andy', 'Sheridan')
+# room_01.add_student('Bob', 'Sheridan')
+# room_01.add_student('Craig', 'James')
+# room_01.add_student('Dan', 'Archibald')
+
--- classroom_student_user_input.py
+++ classroom_student_user_input.py
@@ -13,16 +13,18 @@ import class_student
 
 
 def create_new_classroom():
+    # Create a new object from Class classroom with attributes for Class ID and Max class size.
     room_id = input('Insert new classroom ID, eg. room_01, room_02: ')
     max_room_size = input('Insert new classroom maximum size, eg 20, 25: ')
     class_classroom.Classroom(room_id, max_room_size)
 
+student1 = class_student.Student("John", "Sheridan")
+student2 = class_student.Student("Wolfgang", "Amadeus")
+
+# class_classroom.Classroom.add_student('John', 'Sheridan')
+# class_classroom.Classroom.add_student('Wolfgang', 'Amadeus')
 
 
-room_01 = class_classroom.Classroom("room_01", 25)
-room_02 = class_classroom.Classroom("room_02", 40)
 
-student1 = class_student.Student("John", "Sheridan", 2003, [34, 56, 89, 73, 67])
-student2 = class_student.Student("Wolfgang", "Amadeus", 1770, [13, 47, 36, 77])
-
-create_new_classroom()
+# room_01 = class_classroom.Classroom("room_01", 25)
+# room_02 = class_classroom.Classroom("room_02", 40)
